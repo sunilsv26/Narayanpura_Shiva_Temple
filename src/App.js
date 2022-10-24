1984117623cfaef487afe577bfde1bb55a1728d7
sunil.sherikar <sunil.sherikar@azentio.com>
2022-10-24 15:41:42 +0530

    fixed deployment issue

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import "./assets/scss/index.scss";
 import Main from "./pages/Main";
 import { BrowserRouter, Routes, Route, Redirect } from "react-router-dom";
-import "primereact/resources/primereact.min.css";                  //core css
-import "primeicons/primeicons.css";   
-import 'primeflex/primeflex.scss';
-import "./utils/i18n"
+import "primereact/resources/primereact.min.css"; //core css
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.scss";
+import "./utils/i18n";
 
 function App() {
   return (
     <div className="page-root">
       <div className="page-content">
-        <BrowserRouter>
+        <BrowserRouter basename="/Narayanpura_Shiva_Temple">
           <Routes>
-            <Route  path="/" element={<Main />} />
+            <Route path="/" element={<Main />} />
           </Routes>
         </BrowserRouter>
       </div>
